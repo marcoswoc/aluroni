ee8d1f26cf7785f2996bda325a3639cdedc899c1
Marcos Wellington <marcoswoc@gmail.com>
2022-08-08 19:27:03 -0300

    estilo tema

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import filtros from "./filtros.json";
+import React from 'react';
+import filtros from './filtros.json';
 import styles from './Filtros.module.scss';
 import classNames from 'classnames';
 
@@ -32,5 +32,5 @@ export default function Filtros({filtro, setFiltro}: Props) {
         </button>
       ))}
     </div>
-  )
+  );
 }
